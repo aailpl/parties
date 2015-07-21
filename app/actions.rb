399f99807e557576224c8cf8e185b7b747eefdb6
angelus380 <angelusdelmar@gmail.com>
2015-07-21 12:18:22 +0100

    party-view

--- app/actions.rb
+++ app/actions.rb
@@ -4,3 +4,28 @@ get '/' do
   erb :index
 end
 
+# show individual post
+get '/view/:id' do
+  @party = Party.find(params[:id])
+  erb :view
+end
+
+# form to edit a single party
+get '/:id/edit' do
+ 
+end
+
+# form to create a new party
+get "/new" do
+  
+end
+
+# method to save a new party, the /new route should point here
+post '/create' do
+  
+end
+
+# method to update an existing party, the /:id/edit should point here
+post '/:id/update' do
+  
+end
